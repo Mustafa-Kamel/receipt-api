5ae8c6ce593b24074e86a444b512fb2b8c0e37d4
Mustafa K. Waly <mustafa.kamel@estafsar.com>
2021-10-20 03:36:00 +0200

    Separate the logic to discountable trait

--- app/Http/Resources/ItemCollection.php
+++ app/Http/Resources/ItemCollection.php
@@ -3,12 +3,10 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
-use App\Models\ItemType;
-use App\Models\Item;
-use App\Models\Offer;
 
 class ItemCollection extends ResourceCollection
 {
+    use discountable;
     public static $wrap = 'items';
     private $subtotal = 0;
     private $shipping = 0;
@@ -36,83 +34,5 @@ class ItemCollection extends ResourceCollection
                 "Total" => "$" . $this->total()
             ]
         ];
-    }
-
-    private function run_calculations()
-    {
-        $this->offers = Offer::whereNull('applied_on_id')->get();
-        foreach ($this->collection as $item) {
-            $this->subtotal += $item->price * $item->count;
-            $this->shipping += ($item->weight / $item->country->ship_weight) * $item->country->ship_rate;
-            $item_offers = $item->offers()->where('count_range_min', '<=', $item->count);
-            $this->offers = $this->offers->merge($item->itemtype->offers()->union($item_offers)->get());
-        }
-        $this->vat = $this->subtotal * env('VAT', 0.14);
-        $this->apply_discounts();
-    }
-
-    private function total()
-    {
-        return array_sum(
-            [
-                $this->subtotal,
-                $this->shipping,
-                $this->vat,
-                -$this->discounts_sum
-            ]
-        );
-    }
-
-    private function apply_discounts()
-    {
-        foreach ($this->offers->groupBy('applied_on_type') as $itemTypeOffers) {
-            foreach ($itemTypeOffers as $offer) {
-                if ($this->is_offer_applicable($offer)) {
-                    $value = $this->get_discount_value(
-                        $offer->discount_type,
-                        $offer->discount_value,
-                        $this->get_discountable($offer)
-                    );
-                    $this->discounts[$offer->title] = '-$' . $value;
-                    $this->discounts_sum += $value;
-                }
-            }
-        }
-    }
-
-    private function is_offer_applicable($offer)
-    {
-        return (
-            ($offer->applied_on && $this->is_itemtype_items_applicable_for_discount($offer))
-            or
-            (is_null($offer->applied_on_id) && $this->is_all_items_applicable_for_discount($offer)));
-    }
-
-    private function is_itemtype_items_applicable_for_discount($offer)
-    {
-        if ($this->collection->where('type_id', $offer->applied_on->id)->count() >= $offer->count_min_range)
-            return True;
-        return False;
-    }
-
-    private function is_all_items_applicable_for_discount($offer)
-    {
-        if ($this->total_items_count >= $offer->count_range_min)
-            return True;
-        return False;
-    }
-
-    private function get_discountable($offer)
-    {
-        return ($offer->is_shipping_discount()) ?
-            $this->shipping : ($offer->discount_on->price) ??
-            $this->collection->where('type_id', $offer->discount_on->id)->first()->price;
-    }
-
-    private function get_discount_value($type = 'FIXED', $value, $discountable)
-    {
-        if ($type == 'PERCENT')
-            return $value * $discountable;
-        return $value;
     }
 }
