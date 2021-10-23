f135e45a2a9ef3185f488c2a8ea05a8671650d88
Mustafa K. Waly <mustafa.kamel@estafsar.com>
2021-10-23 18:50:55 +0200

    Rename valid_to column to expires_at

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -9,9 +9,8 @@ class Offer extends Model
 {
     protected $table = 'offers';
     protected $fillable = [
-        'title', 'valid_from', 'valid_to', 'applied_on_type', 'applied_on_id',
-        'count_range_min', 'count_range_max', 'discount_on_type', 'discount_on_id', 'discount_type',
-        'discount_value'
+        'title', 'applied_on_type', 'applied_on_id', 'count_range_min', 'count_range_max',
+        'discount_on_type', 'discount_on_id', 'discount_type', 'discount_value', 'valid_from', 'expires_at'
     ];
 
     public function appliedOn()
--- database/migrations/2021_10_17_112100_create_offers_table.php
+++ database/migrations/2021_10_17_112100_create_offers_table.php
@@ -16,14 +16,14 @@ class CreateOffersTable extends Migration
         Schema::create('offers', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->timestamp('valid_from');
-            $table->timestamp('valid_to');
             $table->nullableMorphs('applied_on');
             $table->unsignedSmallInteger('count_range_min');
             $table->unsignedSmallInteger('count_range_max')->nullable()->default(65535);
             $table->nullableMorphs('discount_on');
             $table->enum('discount_type', ['FIXED', 'PERCENT'])->default('FIXED');
             $table->decimal('discount_value')->nullable()->default(0.0);
+            $table->timestamp('valid_from');
+            $table->timestamp('expires_at');
             $table->timestamps();
         });
     }
